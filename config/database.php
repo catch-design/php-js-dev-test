fe63f9754888f890f8029e65327b71ec39eecf7b
Richard Davies <richardpdavies@yahoo.co.nz>
2016-06-08 07:56:35 +1200

    Bugfix

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', ''),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'normal'),
-            'username' => env('DB_USERNAME', 'normal'),
-            'password' => env('DB_PASSWORD', 'asdasdasd'),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
