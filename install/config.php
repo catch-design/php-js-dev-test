45430cb23ed70ebde41955b92afab92c5c67d539
Varun Sankar <37424416+sivav1@users.noreply.github.com>
2018-07-07 17:11:44 +1200

    primary installation configuration

---
+++ install/config.php
@@ -0,0 +1,41 @@
+<?php
+class config {
+    public $isInstalled = false;
+    public $db_host = "";
+    public $db_username = "";
+    public $db_password = "";
+    public $db_name = "";
+
+    public function __construct(){
+        $this->loadConfig();
+    }
+
+    public function loadConfig() {
+        $string = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/catchnz-test/config.json');
+        $json_a = json_decode($string, true);
+
+        $this->isInstalled = $json_a['isInstalled'];
+        $this->db_host = $json_a['db_host'];
+        $this->db_username = $json_a['db_username'];
+        $this->db_password = $json_a['db_password'];
+        $this->db_name = $json_a['db_name'];
+    }
+
+    public function toJson(){
+        return json_encode(
+            array(
+            'isInstalled'=> $this->isInstalled,
+            'db_host'=> $this->db_host,
+            'db_username'=> $this->db_username,
+            'db_password'=> $this->db_password,
+            'db_name'=> $this->db_name
+        ));
+    }
+
+    public function saveConfig(){
+        $fp = fopen($_SERVER['DOCUMENT_ROOT'] . '/catchnz-test/config.json', 'w');
+        fwrite($fp, $this->toJson());
+        fclose($fp);
+    }
+}
+?>
