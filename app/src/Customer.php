ffcf67721c2ef05890b0f8df8ddfefa9b9fb21b9
Cameron Grant <cam@sasky.nz>
2018-08-08 17:56:01 +0100

    getting json customer endpoint test to pass

--- app/src/Customer.php
+++ app/src/Customer.php
@@ -18,4 +18,29 @@ class Customer extends DataObject
     ];
 
     private static $table_name = 'Customer';
+
+    public static function get_all_records()
+    {
+        $records = [];
+        foreach (self::get() as $customer) {
+            $records[] = $customer->getRecord();
+        }
+        return $records;
+    }
+
+    public function getRecord()
+    {
+        $record = [];
+        $record['id'] = $this->ID ? $this->ID : '';
+        $record['first_name'] = $this->FirstName ? $this->FirstName : '';
+        $record['last_name'] = $this->LastName ? $this->LastName : '';
+        $record['email'] = $this->Email ? $this->Email : '';
+        $record['gender'] = $this->Gender ? $this->Gender : '';
+        $record['ip_address'] = $this->IPAddress ? $this->IPAddress : '';
+        $record['company'] = $this->Company ? $this->Company : '';
+        $record['city'] = $this->City ? $this->City : '';
+        $record['title'] = $this->Title ? $this->Title : '';
+        $record['website'] = $this->Website ? $this->Website : '';
+        return $record;
+    }
 }
