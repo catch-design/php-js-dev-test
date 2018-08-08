d64ba0e2fdff8d7b5fad52c97d62588affe06011
Cameron Grant <cam@sasky.nz>
2018-08-08 17:00:39 +0100

    Bit more Setup

--- app/_config.php
+++ app/_config.php
@@ -3,6 +3,14 @@
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
 
+if (isset($_GET['d']) && ($db = $_GET['d'])) {
+    global $databaseConfig;
+    if ($db == 'l') {
+        $databaseConfig['type'] = 'SQLite3Database';
+        $databaseConfig['path'] = ':memory:';
+    }
+}
+
 // remove PasswordValidator for SilverStripe 5.0
 $validator = PasswordValidator::create();
 $validator->setMinLength(8);
